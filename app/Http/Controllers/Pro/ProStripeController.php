5fb40cd259fef94fbbbc217e778ef08e766b8732
WebDeveloper <2018-cs-2012@uosahiwal.edu.pk>
2023-03-07 20:04:02 +0500

    add stripe path saqib

--- app/Http/Controllers/Pro/ProStripeController.php
+++ app/Http/Controllers/Pro/ProStripeController.php
@@ -9,8 +9,30 @@ class ProStripeController extends Controller
 {
     public function stripe()
     {
-      ////  require '../../stripe/vendor/autoload.php';
-    ////  require_once(__DIR__.'../../vendor/autoload.php');
-        dd("check");
+      
+      require base_path('app/Http/stripe/vendor/autoload.php');
+      
+
+        \Stripe\Stripe::setApiKey('sk_test_51L2veGCBlNnwq5kHyAccYGFUzxhaxyzGka088URwY3RtNFwTHiv6ayVbuwmxkAOeaOTW8C3q6MWovIOjD9HVBQua00RZr8tb4U');
+        $checkout_session = \Stripe\Checkout\Session::create([
+            'success_url' => 'http://localhost/stripe_sub/success.php?session_id={CHECKOUT_SESSION_ID}',
+            'cancel_url' => 'http://localhost/stripe_sub/cancel.html',
+            'payment_method_types' => ['card'],
+            'mode' => 'subscription',
+            'line_items' => [[
+            'price' => "price_1M52beCBlNnwq5kHAzcq0YJR",
+            // For metered billing, do not pass quantity
+            'quantity' => 1,
+            ]],
+        ]);
+
+   echo "<pre>";
+   print_r($checkout_session);
+   echo  "</pre>";
+
+      
+      
+      
+      dd("check");
     }
 }
